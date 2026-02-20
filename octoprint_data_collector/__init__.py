7d87fa43421ac1c105838ee05aeae6bcfb2947ba
Alan Ocho <alanocho@Alans-MacBook-Pro.local>
2026-02-20 19:26:48 +0800

    data: removed unnecessary lines

--- octoprint_data_collector/__init__.py
+++ octoprint_data_collector/__init__.py
@@ -141,20 +141,12 @@ class DataCollectorPlugin(
 
         self._running = True
 
-        _, printer_port, _, _ = '/dev/ttyACM0'#self._printer.get_current_connection()
-
         # auto find ports
         potential_ports = glob.glob('/dev/ttyACM*') + glob.glob('/dev/ttyUSB*')
 
-        
         self._logger.info(f"Found USB ports: {potential_ports}")
-        self._logger.info(f"Found printer ports: {printer_port}")
 
         for port in potential_ports:
-            if port == printer_port:
-                self._logger.info(f"Skipping main printer port: {port}")
-                continue
-
             reader = SerialSensorReader(
                 port=port,
                 shared_data=self._latest_data,
